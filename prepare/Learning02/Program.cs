b3418de213b31ec343142102447040350f9d835b
NhicolasAcosta <aco21007@byui.edu>
2023-04-28 10:42:39 -0300

    w2-learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "community manager";
+        job1._company = "Mercado Libre";
+        job1._startYear = 2014;
+        job1._endYear = 2016;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Marketing manager";
+        job2._company = "Mercado Libre";
+        job2._startYear = 2016;
+        job2._endYear = 2020;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jimena Rodriguez";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
